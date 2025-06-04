68b33978571c49ebae8d043c52f2749d4d57839c
Prajna <prajnapoojaryr@gmail.com>
2025-06-04 08:46:44 +0530

    first commit

---
+++ src/main/java/mavengradle4/prg10/App.java
@@ -0,0 +1,13 @@
+package mavengradle4.prg10;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
